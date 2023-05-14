ac03fa4a63a2f09048c306483f575aeae8fbe52a
kavinda keshara <kavindakeshara97@gamil.com>
2023-05-14 16:12:32 +0530

    frontend

---
+++ src/Containers/footer/footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import SocialMediaHorizontal from '../SocialMedia.js/SocialMediaHorizontal/SocialMediaHorizontal'
+import './footer.css'
+
+function footer() {
+
+    return (
+        <>
+            <footer className="fadeInUp col-md-12">
+         
+
+
+                <SocialMediaHorizontal />
+            </footer>
+            <h5 className="text-center copyright"> © Copyrights reserved by Keshara1997</h5>
+        </>
+    )
+}
+export default footer
